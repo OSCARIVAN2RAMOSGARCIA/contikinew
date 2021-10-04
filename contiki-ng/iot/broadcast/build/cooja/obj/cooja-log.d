9a00ee63cc1bb9acfbf21bb0f4f639d0137842a4
OSCARIVAN2RAMOSGARCIA <oramos6@ucol.mx>
2021-10-03 22:30:32 -0500

    Contiki broadcast

---
+++ contiki-ng/iot/broadcast/build/cooja/obj/cooja-log.d
@@ -0,0 +1,8 @@
+¡#
+0IÞÌbk°ÂÄ›Ç©+8½•…7§ÓwW~eÕeb°#õP¶FÇñšV#MÕ/-W;Á“¹°3"0‹k#ƒ„°m	™à©yqÇ{ã¿(”L’Dó“t&Ÿ‰ÌW6lN—o°/w9ûpÎèº¸”TÝøg‹°¬œ‘Žž:¯ZÊ)¹9syù2Ú¼s‚sia<?…ÆªœžÖØAîA«$¦ÖH±Ä÷cÔRÓÓrzqÉ¶iµöCˆ	Á¡÷@yÀN)Îíi¯¸æëŒ‡GÒHMÁV]EÞ˜Ú×–²û#I$G£^å>]¸¬¤±Úxßc[;©–Ð›·Î<èží¨=Ýi]ÓÔŠ÷³ä”8*‰¯zR'lÛ‰>W–4S^üÿ¤’øÝKJÿÛ?V€
+˜bµžJÏ=]¼oÖ–qïšà+P{u²ÜX ¬úb—’,D°,¶¢ð`W2ËtWD—_2Ÿ/_Þ#9‹u6OF)>œýEö“s‚JêÌ-|û¶¼•³zçç©¸º„²›»ŠIÓÉ±p?æ{ñ¹RoÊÒTÎuòrÇùç'Ã~¸Ä9Uz0B·³*uœ/~Õ’Uñ6JJ±–—Áâ.>£UÚÐ¦íA*Ùt"«&õÍøÉÒîâ*Qú!Üiž²ÊS:;T/I(l±¨]ÎDþã,|ñîídïª£v¹$=}v´!£Ú—Ÿ!Û™BñáÌŸ¦Móƒln¯»‹¹-¦<:>1ãÃÈÀÖ\'S¥#šBÅZ:ÜdJØqÕí'Œ}à#ÙÒS$@›Îê
+“¤U"wïM´ø_./¯wV„Ö{ÈîÙ.uBD×9`ÜŽújÎÏnx²ó’7°<RšÒµîvhâ‰su‘Â—(MX)òY¹ã1C,~ï#?Ó¶Ìi¨¹ê=&ÔK:Ð×¸¸ŠÒ
+ë06Ï`PÍGwQM9½!p©0·¤X#0à¾ŽÜp¥‚Ús/ì_•i´é\ª’ªÚ—ª¤v9™5-sËÅ?–ÜÝèr#haU$Ã:|î˜p¹ ‹¸(=’vPžIE—: TDÂ‡Ñ·Ã.qJÐp¥ü\žåÂüÔ´#$4o+«Z÷¨šëUÂJ&™T€Ç­i
+úLÇWä³]æ¯üE›æ é¡0ÂvN*õ8êg™]Ê³ªDñ0ò,y‘èª¢ÜÙ‚q»L;7²ˆ³Gžë
+·¹áƒþ|hlJG{¡øe|	~wõŸKÏL-;Õ§MŸMt,ˆÆ(ZA“­	îp%3KÉËá2nï,6¡1hbÐB/‘ŒŒ8áw$pzb²ï'«ÀU…¶H–BÕL3x“m×Ž;~Õœ{•{JìøWêG3ñDOÃ‹Ò:¾ü›"Z/ØÇükrèÅ	@Ž#‡šØiÁ’»í~‡ð)ŒQÖ\;šçôñ9>U'±µ„$à@­†XÚÉËÌ‘[˜Èö9¡¥‡$lŸ+»œéA×¬”+§ècµ´ñ¸ù02¢=Êý^Pã»'£ø†J3]í‚RaØk8MÖNCÆmuuƒ”~t3bïwtvcrS()
+sS“uà%\So5~¾Øãëìš•Èt ³°Ô	eNaoìG~QßHººYç™$EÝžõYÜý¤!Ì¿ï	›ŠÞÊ¤h²¬’sBGC)'‚¤­’£$Ðãg½»açûñë€!ÜEä~	Áóá5iñ£¢öÕ0–*&}ÛÒ7ž¤àä.ºó·ù"‘§^CÕÝ6öÙ¶[å8\O³]õ£:É—É‹!N>·g¥§G­Ù3‚”om›H6a‰mXðNãÔÚËlY…¾aÍ…tô["ß˜ „ ]¼üž<(—%ˆ!rÇxô*cyð¬6/#‘{V©5ô#ôž)w­êÝu¬ë3å¾»®äq©å«IçôDÐûHÒd©ýmôoàâ¸õ~P»E8.UàµZ±ßÂÀ§_„/ˆV8åäÄª™ó·[žö‡ÄSh1ìŒkÊ¹Ù+¢”¨kw
---
+++ contiki-ng/iot/broadcast/build/cooja/obj/leds.d
@@ -0,0 +1,7 @@
+~Vä\<ëç”²í‘s%ˆ9'²N³²QÊ]vTNLzÎÌŠoNM­GÛMÿb/¾«œžZºœÏ/3ÿ2ÏsÍý4¾ñZ…zhÆ)
+—Ï9rÇIô˜³úõYeS.Q¨‰º°Â*u
+"pªmŒ‹ZVDÔ(leååuÔµêU‘âœ°Il8cPå”zùrÎÁàþÎ\Kuõt/;'½MËÒ«’t0cÿR'0sÕÓEýÊú@Ïíõw¤rb»a¨²§ãÔ—ãJm{’“]/ÈÙþ'»ò{Å”ó>Ê	>3Tñ§+øÊ:çžúOå3¿·æÕœyD:ç}¦E*êcQ¹þúÍbÉ²³=±‘éž:ÍOêNø’lm|ÄrÊNþÚÒ|-ç¾úŠõ-àœ}­2§ßouüˆ2+á6);qÔÜ­n§-ß9];wÒç"u<é§]`çÝ¦|/M­™7Ù©÷›Þî•Ávùb ;}%Èß2S§¿øÎ`b'`JXgpŸ¬S°gµs8¬ÝI<Ùî,ŸÑ;iÎÃ‹ó'âi3±î­©¸2ß\,Éï2wk•;Ž5ïã8qçÙ°Ù	Y4Þ,²ê”Lòíœwå;)‰éÎÊÌNËçÔe+*;1m±ÎÌÇ»¿föbvn·y¡8³Å¶³³†·Ó(ëüÖÓw‚	É¡¾å§ès9zä×IZØYTØiZ¼ßž^7w¨îÔ¤òÎêŠu§5É·sº„ÜÉŽ,w¶‰®K";_=|°v]ÎØ²Ø§ì™lvvÏÑ;i‡¿ÎÛå÷\§3÷d¿©;FÖÜ}®ïb-AßÛ¬õ§¥[‘NP“Ënµ³ËD«NÈš^y<w~âwF]žùƒEƒT6LÊÕŠ#†ŠTdÙ295Ëž;ÍJFüÕY;ëD’»“­3¥3µÄ~sMÅ¸ÀæÛØA:ºq6Ý•Æ¿Û·‡(Ù	²ØµÓåjçÜcQg˜.ñr’UœÓÁïÒÑÊt¬óƒì´îÞ‘ðwñÌ`;æQ‹Û)½·§Û¹·©ÖÉf“vgO‘»³i´SÆ+ãíô«#ìúë<¢NÀ2Êœ¸ÕªÀö™u
+Šéì8hÔ9áhx'ágvgQO§† ÁCÃ¬ÎÄ´TDdí™xDÝT|éî	”ÙsÑq¬“ñä¸g¨)~T£|4êtôHñ_epîI¢(Ù³„é²NSDÓV',	¶z$v'rì»3Í¨âÞâÎ·–°ÓÛìØùÜÒÜ	W$Ùø˜±SÜLV;$nï§KwÆèq3{ç9åY
+“¯ê:ÊÝúrÂnç²p«Sò»sùæßIYRz[–‚W¼üôïbš
+uÌü[íÒ¬&®›Ü·œJHÕôü\îü”šë6ß}u‚†èw†Z•u‘¬ÎQÈÓNR"²U¥µÓî4ŽÕvžÂqu¢šÁw¦ír:Uí¶¥Vë£-lõ3ëtµ%î½ÚF¬9ª¤bÙ‹³NY°Ô«ÎÁMQõ<GjtçÙ;£ûDÒy¬Îé=Ë'õ1sYýWÙ´^„í]oQP‰ýøÚÌþ]ï´—s®m¥[â®/©gw¬yéÝb×ùÉÞoVé
+¿©S|G»ÿlí’/àwåíÜivž“à;Ñ…Áþôè¾RžQÖ¹^´ÞÉOIël¯“X¹ÜùDwÂÕÊñY|+ùNtwÒÍtVó§xuÒë±:ëý°ö€~¨ûÏÆïÄŽ=áÇ6žqDî§Ü—s®íÑI·“ìÖå©{vñžÝ@ŽçÞÈÑ?Ì[^öõÑþ¹wHä“¯TÞÙ·îô{÷ÿP=›Á=ÀŒ3ÏàH´§ð©Üs,Fö$¯¹=lOÃoyø×èD¼ôLì›ô*Þ&{‹ÅÔžc<“'Æ™Ûtsó?îPÿ	¹³èùiï”üeöNà{)g…=ÊfhÏå±§9Påyì·ôÄ>ô{fž=5‡Bo7ç¶'yQîÙÖêÛzó¢êA?=¡CÈžÑ!hOèÐ—çéÒ›'iÈ°gô„ßÓzDìyMâôDîy¦žü8¨çjÀŒ'kD³ª5Û	¢\ÚOäù»Ëê	3çÝ÷”Æ¸zÎ÷!=i‹<kýêý¶íç÷¼ž¨yâûõ¢rî	T”îVâ{bM
